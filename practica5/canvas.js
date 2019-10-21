334f82cb5f97e4797facfe9d7be7692b94dd5f76
Javier Frías <javifs94@gmail.com>
2019-10-21 17:54:02 +0200

    Actualización práctica 5

--- practica5/canvas.js
+++ practica5/canvas.js
@@ -15,12 +15,39 @@ window.onload = function() {
     // Incluimos el elemento canvas
     contexto = cargarContextoCanvas('micanvas');
     if (contexto) {
-        let img = new Image();
-        img.src = 'perros.jpg';
-        img.onload = function() {
-            contexto.drawImage(img, 400, 225, 160, 234, 15, 15, 120, 180);
-            //tamaño natural
-            contexto.drawImage(img, 160, 20, 500, 400);
-        }
+
+        imagen();
+        circulo();
+
+
     }
+}
+
+/**
+ * 
+ */
+function imagen() {
+    let img = new Image();
+    img.src = 'perros.jpg';
+    img.onload = function() {
+        contexto.drawImage(img, 400, 225, 160, 234, 15, 15, 120, 180);
+        //tamaño natural
+        contexto.drawImage(img, 160, 20, 500, 400);
+    }
+
+    /**
+     * 
+     */
+    function circulo() {
+        let X = micanvas.width / 2;
+        let Y = micanvas.height / 2;
+        let r = 75;
+        contexto.strokeStyle = "#006400";
+        contexto.fillStyle = "#6ab150";
+        contexto.lineWidth = 5;
+        contexto.arc(X, Y, r, 0, 2 * Math.PI);
+        contexto.fill();
+        contexto.stroke();
+    }
+
 }
