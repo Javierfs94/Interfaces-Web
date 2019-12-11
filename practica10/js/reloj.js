f41a9ed2e37080332a117c340b640b2f02758a79
Javier Frías Serrano <javifs94@gmail.com>
2019-12-11 20:05:31 +0100

    Actualizacion repositorio

---
+++ practica10/js/reloj.js
@@ -0,0 +1,12 @@
+function reloj() {
+    momento = new Date()
+    hora = momento.getHours()
+    minuto = momento.getMinutes()
+    segundo = momento.getSeconds()
+
+    horaImprimible = hora + " : " + minuto + " : " + segundo
+
+    document.form_reloj.reloj.value = horaImprimible
+
+    setTimeout("reloj()", 1000)
+}
